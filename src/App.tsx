4b2dfb8803534bd963661dcd97d3e26dbf8ed90a
Patryk <patrykadamczyk@paipweb.com>
2019-02-14 09:43:00 +0100

    console.debug z wersją

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import packageJSON from '../package.json';
 
 export default class App extends Component {
   render() {
+    console.debug(`Patryk Adamczyk Portfolio v${packageJSON.version}`);
     return (
       <div className="App">
         <h1>Hello World!</h1>
